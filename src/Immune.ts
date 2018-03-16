cae4955e706d6f819d9e3f6b2d501d7963d636a4
Sandy Gifford <sanford.r.gifford@gmail.com>
2018-03-15 21:42:15 -0400

    organization

--- src/Immune.ts
+++ src/Immune.ts
@@ -3,34 +3,54 @@ import * as Immutable from "immutable";
 type ImmutablePath = (string | number)[];
 
 export class Immune {
-	public static makeImmune<T extends {}>(obj: T, onChange: (obj: T) => void): T {
-		return this.makeSubImmune(Immutable.fromJS(obj), [], onChange);
+	public static makeImmune = <T extends { [key: string]: any }>(obj: T, onChange: (obj: T) => void): T => {
+		return Immune.makeSubImmune(Immutable.fromJS(obj), [], onChange);
 	}
 
-	private static makeSubImmune<T extends {}>(baseImmutableObj: Immutable.Map<any, any>, path: ImmutablePath, onChange: (obj: T) => void): T {
-		return baseImmutableObj.getIn(path).reduce((immuneObj: {}, value: any, key: string) => {
+	public static areEqual = <T>(immuneObj1: T, immuneObj2: T): boolean => {
+		return null;
+	}
+
+	public static getImmutable = (obj: { [key: string]: any }): Immutable.Iterable<any, any> => {
+		return null;
+	}
+
+	public static demune = <T extends { [key: string]: any }>(obj: T): T => {
+		return null;
+	}
+
+	private static makeSubImmune<T extends {[key: string]: any}>(baseImmutableObj: Immutable.Map<any, any>, path: ImmutablePath, onChange: (obj: T) => void): T {
+		const immuneObj: T = baseImmutableObj.getIn(path).reduce((immuneObj: {}, value: any, key: string) => {
 			const propPath = path.concat([key]);
+
 			Object.defineProperty(immuneObj, key, {
-				get: () => {
-					const val = baseImmutableObj.getIn(propPath);
-					switch (typeof val) {
-						case "boolean":
-						case "undefined":
-						case "number":
-						case "string":
-						case "symbol":
-							return val;
-						default:
-							return this.makeSubImmune(baseImmutableObj, propPath, onChange);
-					}
-				},
+				get: Immune.propertyGetter(baseImmutableObj, propPath, onChange),
 				set: (val) => {
-					onChange(this.makeSubImmune(baseImmutableObj.setIn(propPath, val), [], onChange));
+					onChange(Immune.makeSubImmune(baseImmutableObj.setIn(propPath, val), [], onChange));
 				},
 				enumerable: true,
 				configurable: false
 			});
 			return immuneObj;
 		}, {} as T);
+
+		return immuneObj;
+	}
+
+	private static propertyGetter(baseImmutableObj: Immutable.Map<any, any>, path: ImmutablePath, onChange: (obj: any) => void): () => any {
+		return () => {
+			const val = baseImmutableObj.getIn(path);
+
+			switch (typeof val) {
+				case "boolean":
+				case "undefined":
+				case "number":
+				case "string":
+				case "symbol":
+					return val;
+				default:
+					return Immune.makeSubImmune(baseImmutableObj, path, onChange);
+			}
+		}
 	}
 }
